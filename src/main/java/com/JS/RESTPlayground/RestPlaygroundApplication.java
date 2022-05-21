44ed4c1921570d2de195061839afb5692ddfde01
Jonathan Seifert <jonathanseifert1998@gmail.com>
2022-05-21 11:39:03 +0200

    Initial spring setup

---
+++ src/main/java/com/JS/RESTPlayground/RestPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.JS.RESTPlayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPlaygroundApplication.class, args);
+	}
+
+}
