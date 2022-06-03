2965111cf321b38c2ff0acfdfa5266247b846391
Jonathan Seifert <jonathanseifert1998@gmail.com>
2022-06-03 13:09:36 +0200

    initial React.js app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>REST-Playground</h3>
+    </div>
+  );
+}
+
+export default App;
